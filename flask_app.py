2261d9b34c42604394f9157ca2e6a43eee884acb
Abbey <abbeyokgo@gmail.com>
2022-06-22 17:31:29 +0800

    add

--- flask_app.py
+++ flask_app.py
@@ -10,18 +10,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 # api
-
-
 @app.route('/api', methods=['POST'])
 def api():
+    name = request.form['name']
     length = request.form['length']
     width = request.form['width']
     height = request.form['height']
     # 处理逻辑
     # 这里可以对接模型进行计算，然后返回给前端，前端接收到结果可以进行展示
     # 处理逻辑
-    msg = f'长:{length} 宽:{width} 高:{height}'
+    msg = f'Name:{name} Length:{length} WIdth:{width} Height:{height}'
     return jsonify({'msg': msg})
 
 
